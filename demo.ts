085bef2057d4497329aa711ead1bbfadfab7fe09
Ido <idorev@post.bgu.ac.il>
2021-12-26 21:05:56 +0200

    added file reading

--- demo.ts
+++ demo.ts
@@ -1,21 +1,24 @@
-import * as T from "./types";
-import Client from "./client";
-const operations: T.UpdateOperation[] = [
-  { opName: "insert", char: "d" },
-  {
-    opName: "delete",
-    index: 0,
-  },
-];
+// import * as T from "./types";
+// import Client from "./client";
+const fs = require("fs");
 
-const cltData: T.ClientData = {
-  id: 1,
-  port: 8050,
-  local_replica: "abc",
-  clients: [],
-  operations: operations,
-};
+fs.readFile(process.argv[2], (err, data) => {console.log(data.toString().split("\r\n"))})
+// const operations: T.UpdateOperation[] = [
+//   { opName: "insert", char: "d" },
+//   {
+//     opName: "delete",
+//     index: 0,
+//   },
+// ];
 
-const clt:Client = new Client(cltData);
-clt.start();
+// const cltData: T.ClientData = {
+//   id: 1,
+//   port: 8050,
+//   local_replica: "abc",
+//   clients: [],
+//   operations: operations,
+// };
+
+// const clt:Client = new Client(cltData);
+// clt.start();
 
